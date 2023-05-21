d3f9bc865e4c73d309b10a0cac20a1d93dfc1d68
wolfgunblood <jalajdorai1@gmail.com>
2023-05-21 19:10:01 +0530

    Main styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+// import "./"
+import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <Navbar />
+    </div>
   )
 }
 
