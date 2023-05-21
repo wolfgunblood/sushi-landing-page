70d25e0c21edc4f5c2911716d6a17641fda80b4f
wolfgunblood <jalajdorai1@gmail.com>
2023-05-21 20:37:07 +0530

    Updated Hero section

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import "./Hero.scss"
+import Sushi1 from  "../../assets/sushi-1.png";
+
+
+const Hero = () => {
+  return (
+    <section className='hero-container'>
+        <div className='hero-img'>
+            <img src={Sushi1} alt="Sushi One" />
+            <div className='hero__overlay'></div>
+        </div>
+        <div className='hero-text'>
+            
+        </div>
+    </section>
+  )
+}
+
+export default Hero
