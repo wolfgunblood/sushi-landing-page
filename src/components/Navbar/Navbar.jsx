1de6e76f2df59ace15684a7f1ce071e2b2ce55e0
wolfgunblood <jalajdorai1@gmail.com>
2023-05-21 19:58:42 +0530

    Updated NavBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.scss';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='navbar'>
+        <div className='navbar__head'>
+            <h2>SUSHIMAN</h2>
+            <div className='navbar__overlay'></div>
+        </div>
+        <ul className='navbar__list'>
+            <li>MENU</li>
+            <li>FOOD</li>
+            <li>SERVICES</li>
+            <li>ABOUT US</li>
+        </ul>
+    </nav>
   )
 }
 
