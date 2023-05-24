1b2a535fd39eb23914f03bfc7ae75f5dbe1f2ecb
wolfgunblood <jalajdorai1@gmail.com>
2023-05-24 19:07:03 +0530

    Updated Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,42 @@
 import "./Hero.scss"
-import Sushi1 from  "../../assets/sushi-1.png";
-
+import Sushi1 from "../../assets/sushi-1.png";
+import circle from "../../assets/play-circle.svg"
 
 const Hero = () => {
   return (
     <section className='hero-container'>
-        <div className='hero-img'>
-            <img src={Sushi1} alt="Sushi One" />
-            <div className='hero__overlay'></div>
+      <div className='hero-img'>
+        <img src={Sushi1} alt="Sushi One" />
+        <h2 className="hero-kanji">
+          日  <br />
+          本  <br />
+          食
+        </h2>
+        <div className='hero__overlay'></div>
+      </div>
+
+      <div className='hero-content__container'>
+        <div className="hero-info">
+          <h1>Feel the taste of Japanese Food</h1>
+          <p>Feel the taste of Japanese Food from anywhere in the world</p>
+          <div className="hero-button">
+            <button className="hero-button__order">Order Now</button>
+            <button className="hero-button__play">
+              <img src={circle} alt="Play" />
+              How to Order
+            </button>
+          </div>
         </div>
-        <div className='hero-text'>
-            
+
+        <div className="hero-testimonial">
+          <div className="hero-testimonial__customer">
+            <h2>
+              35 <span> k+</span>
+            </h2>
+            <p>Happy Customer</p>
+          </div>
         </div>
+      </div>
     </section>
   )
 }
