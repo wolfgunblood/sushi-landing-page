9a73bb45f1ad9c28c88c1e1ffbee731724c4d11d
wolfgunblood <jalajdorai1@gmail.com>
2023-05-28 18:05:26 +0530

    Updated Trending Foods section

--- src/components/TrendingFoods/TrendingFoods.jsx
+++ src/components/TrendingFoods/TrendingFoods.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import "./TrendingFoods.scss";
-import LeftSushi from "../../assets/sushi-5.png"
-import RightSushi from "../../assets/sushi-4.png"
+import LeftSushi from "../../assets/sushi-5.png";
+import RightSushi from "../../assets/sushi-4.png";
+import Check from "../../assets/check.svg";
 
 
 const TrendingFoods = () => {
@@ -12,6 +13,20 @@ const TrendingFoods = () => {
                     <p className="upper__subtitle">What’s Trending / トレンド</p>
                     <h3 className="upper__title">Japanese Sushi</h3>
                     <p className="upper__description">Feel the taste of the most delicious Sushi here.</p>
+                    <ul className='upper__list'>
+                        <li>
+                            <img src={Check} alt="check" />
+                        </li>
+                        <li>
+                            <img src={Check} alt="check" />
+                        </li>
+                        <li>
+                            <img src={Check} alt="check" />
+                        </li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
                 </div>
                 <div className='upper__image'>
                     <img src={LeftSushi} alt="sushi1" />
@@ -25,6 +40,14 @@ const TrendingFoods = () => {
                     <p className="lower__subtitle">What’s Trending / トレンド</p>
                     <h3 className="lower__title">Japanese Drinks</h3>
                     <p className="lower__description">Feel the taste of the most delicious Sushi here.</p>
+                    <ul className='lower__list'>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
                 </div>
             </div>
         </section>
